82447c86c1f5ed71637b9019482c285598083cad
XXYXie <xueyan.xie@wustl.edu>
2016-11-30 21:17:17 -0600

    Move user-related files to user folder.

--- user/Logout.js
+++ user/Logout.js
@@ -2,11 +2,11 @@ $("#logout_btn").click(function() {
 	var username = document.getElementById("username").value; // Get the username from the form
 	var password = document.getElementById("password").value; // Get the password from the form
 
-  $.ajax({
-    url: "Logout.php",
-    type: "post",
-    data: {"username": username, "password": password},
-    success: function(data){
+	$.ajax({
+		url: "user/Logout.php",
+		type: "post",
+		data: {"username": username, "password": password},
+		success: function(data){
 			var jsonData = JSON.parse(data)
 			console.log(jsonData.success);
 			if(jsonData.success){
@@ -15,8 +15,7 @@ $("#logout_btn").click(function() {
 				document.getElementById("logout_btn").style.display = 'none';
 				document.getElementById("ShowUser").innerHTML = "";
 				document.getElementById("Login").style.display = 'block';
-
 			}
-     }
-   });
- });
+		}
+	});
+});
